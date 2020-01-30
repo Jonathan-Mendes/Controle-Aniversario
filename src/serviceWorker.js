515e88f4608c95df4956b9bd71d412a48d1b2a1a
Jonathan <jonathan_mendes96@outlook.com>
2020-01-30 16:07:57 -0200

    Add Aplicação PWA

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -135,3 +135,17 @@ export function unregister() {
     });
   }
 }
+
+export const registerServiceWorker = () => {
+  if ('serviceWorker' in navigator) {
+      window.addEventListener('load', function () {
+          navigator.serviceWorker.register('/sw.js').then(function (registration) {
+              // Registration was successful
+              //console.log('ServiceWorker registration successful with scope: ', registration.scope);
+          }).catch(function (err) {
+              // registration failed :(
+              //console.log('ServiceWorker registration failed: ', err);
+          });
+      });
+  }
+}
